7f4ec81f1d54f0323e54923d68724e2a70675637
Alejandro U. Alvarez <alejandro@urbanoalvarez.es>
2013-02-09 11:38:52 +0100

    Standard commenting for User class

--- lib/php/user.php
+++ lib/php/user.php
@@ -5,8 +5,8 @@
  */
 class User{
 	
-	// No se podra acceder a las variables de manera directa
-	var $id;
+	// Use g() or get() to access user attributes
+	var		$id;
 	private $fbuser;
 	private $twuser;
 	private $name;
@@ -17,6 +17,14 @@ class User{
 	private $jtime;
 	private $ltime;
 	
+	/*
+	 *	User constructor, builds an object containing a user with
+	 *	all methods for interacting with it
+	 *	@param	id		Required, set to 0 if you want an anonymous user.
+	 *	@param	name	User name
+	 *	@param	email	User email
+	 *	@return	User object
+	 */
 	function User($id,$name=false,$email=false){
 		if($id>0){
 			$this->set('id',$id);
@@ -51,6 +59,12 @@ class User{
 		}
 	}
 	
+	/*
+	 *	Return a user attribute. It also manages cache through Session variables
+	 *	@param	what	Attribute to be returned
+	 *	@param	db		Whether you want to pull the attribute from the database
+	 *	@return	The value if found, false otherwise
+	 */
 	function g($what,$db=true){
 		// Get data from session
 		if(isset($_SESSION['user'][$what]) && strlen($_SESSION['user'][$what])>0 && $_SESSION['user'][$what]) return $this->set($what,stripslashes($_SESSION['user'][$what]));
@@ -63,16 +77,26 @@ class User{
 		return $this->set($what,stripslashes($db->queryUniqueValue('SELECT '.$what.' FROM users WHERE id = \''.$this->id.'\'')));
 	}
 	
-	// Funcion get, asegura compatibilidad con Chevismo
+	/*
+	 *	Same as g() above
+	 */
 	function get($what){
 		return $this->g($what);	
 	}
 	
-	// Checks if user has picture uploaded
+	/*
+	 *	Checks if user has picture uploaded
+	 *	@return true if user has uploaded a pic, false otherwise
+	 */
 	function hasPic(){
 		return file_exists('img/user/uploads/'.$this->id.'.gif');
 	}
 	
+	/*
+	 *	Returns the associated picture for the current user
+	 *	@param	type	Image type: profile or square
+	 *	@return	The image full src
+	 */
 	function pic($type='profile'){
 		// Devuelve la ruta a la imagen
 		if($type=='profile'){
@@ -135,6 +159,14 @@ class User{
 		}
 	}
 	
+	/*
+	 *	Setter for user attributes
+	 *	@param	what		Attribute to set
+	 *	@param	new			New value to be assigned
+	 *	@param	updateDB	Whether to update the database
+	 *	@param	clean		Whether to strip html tags and escape the string
+	 *	@return	The same thing that was passed to new
+	 */
 	function set($what,$new,$updateDB=false,$clean=true){
 		if($updateDB){
 			global $sess, $db;
@@ -163,7 +195,8 @@ class User{
 	}
 	// Get geolocation for user IP
 	// Returns an array 0->Country 1->Region 2->City
-	function locate($ip){
+	// It's called from getLoc() below
+	private function locate($ip){
 		$fp = @fopen('http://www.ipaddresslocation.org/ip-address-locator.php', 'r', false, stream_context_create(array('http' => array('method' => 'POST','content' => http_build_query(array('ip'=>$ip)) ))));
 		if(!$fp)  return false;
 		$res = @stream_get_contents($fp,-1,300);
@@ -181,7 +214,13 @@ class User{
 		$c = preg_match('@Longitude:</i> <b>(.*)</b>@',$e[0],$data[4]);
 		return array($data[0][1],$data[1][1],$data[2][1],$data[3][1],$data[4][1]);
 	}
-	// Gets location for a given user ID
+	/*
+	 * Gets location for a given user ID
+	 * alternatively you can pass another IP address
+	 *	@param	usid	User id whose location you want
+	 *	@param	ip		Alternative IP to check
+	 *	@return	Array with user location [country, region, city, lat, lng]
+	 */
 	function getLoc($usid,$ip=false){
 		// First check in session:
 		if(isset($_SESSION['user']['loc']) && $usid == $this->id){
@@ -224,6 +263,17 @@ class User{
 			}
 		}
 	}
+	/*
+	 * Returns location of user, if available
+	 * type determines what data to return
+	 * 0 = Country
+	 * 1 = Region
+	 * 2 = City
+	 * 3 = Lat
+	 * 4 = Lng
+	 * @param	type	Check the values above
+	 * @return	The location type requested or false if not available
+	 */
 	function location($type=1){
 		if(isset($_SESSION['user']['loc'])){
 			return $_SESSION['user']['loc'][$type];
@@ -232,26 +282,29 @@ class User{
 			return $loc[$type];
 		}
 	}
+	/*
+	 * Sends private message to user, from current user
+	 * All variables are checked within this function
+	 *	STATUS GUIDE
+	 *		0 -> Just sent, unread
+	 *		1 -> Read
+	 *		2 -> Deleted by sender, unread
+	 *		3 -> Deleted by sender, read
+	 *		4 -> Deleted by receiver
+	 *	IDENT GUIDE
+	 *		# ->	To			Current User
+	 *		0 ->	Public		Public
+	 *		1 ->	Private		Public
+	 *		2 ->	Public		Private
+	 *		3 ->	Private		Private
+	 * @param	to		User ID of receiver
+	 * @param	msg		Message content, no need to clean it
+	 * @param	thread	Thread ID, or 0 if PM is not a reply
+	 * @param	ident	Check description above
+	 * @param	com		If the message is in reply to a comment, the ID of the comment
+	 * @return	ID of the sent PM or false if something failed
+	 */
 	function sendPM($to,$msg,$thread=0,$ident=0,$com=0){
-		// thread indica el hilo al que pertenece
-		// from is always the current user
-		//
-		// $com : ID del comentario al que responde
-		//
-		// STATUS GUIDE
-		//	0 -> Just sent, unread
-		//	1 -> Read
-		//	2 -> Deleted by sender, unread
-		//	3 -> Deleted by sender, read
-		//	4 -> Deleted by receiver
-		//
-		// IDENT GUIDE
-		//	# ->	To			Current User
-		//	0 ->	Public		Public
-		//	1 ->	Private		Public
-		//	2 ->	Public		Private
-		//	3 ->	Private		Private
-		// _____________________________
 		global $sess, $db,$user;
 		if(!$sess->logged()) return false;
 		if(!($db instanceof DB)) $db = $sess->db();
@@ -268,6 +321,12 @@ class User{
 			return $db->lastInsertedId();
 		else return false;
 	}
+	/*
+	 * Set PM to read status. If message is part of a thread
+	 * it sets the status to read for the whole thread.
+	 * @param	thread	ID of the thread if PM is a reply
+	 * @return	Whether status was set
+	 */
 	function updatePMstatus($thread){	
 		// Updates Thread to READ status
 		// Returns false if user has NO permission
@@ -288,9 +347,20 @@ class User{
 		if($nextStatus) $db->execute('UPDATE msg SET status = \''.$nextStatus.'\' WHERE (`thread` = \''.$thread.'\' OR `id` = \''.$thread.'\')  AND `to`=\''.$user->id.'\' AND `status` = \''.$status.'\'');
 		return true;
 	}
+	/*
+	 * Check whether a user has been located
+	 * @return true if user has location, false otherwise
+	 */
 	function hasLoc(){
 		return isset($_SESSION['user']['loc']);
 	}
+	/*
+	 * Link given facebook ID to the current user
+	 * if no ID is provided it checks whether there is
+	 * a logged in facebook account
+	 * @param	fbid	Facebook account ID, if false it will use currently logged fb account
+	 * @return	Whether the account was linked
+	 */
 	function linkFB($fbid=false){
 		global $sess, $db, $fb;
 		if(!($sess instanceof Session)) return false;
@@ -309,6 +379,10 @@ class User{
 		}
 		return $this->set('fbuser',$fbid,true);
 	}
+	/*
+	 * Unlink Facebook account from the current user
+	 * @return	Whether the account was unlinked
+	 */
 	function unlinkFB(){
 		if(!$this->fb()) return true;
 		global $sess, $db, $fb;
@@ -326,21 +400,12 @@ class User{
 		$fb->facebook->destroySession();
 		return $this->set('fbuser',0,true);
 	}
-	function unlinkTW(){
-		if(!$this->tw()) return true;
-		global $sess, $db;
-		if(!($sess instanceof Session)) return false;
-		if(!($db instanceof DB)) $db = $sess->db();
-		if(!$this->fb() && !$this->g('email')){
-			// No puedes quedarte sin cuentas vinculadas
-			$sess->set_msg('Debes tener alguna cuenta vinculada siempre. Vincula Facebook o especifica una contraseña para poder desvincular Twitter');
-			return false;	
-		}
-		unset($_SESSION['user']['twuser']);
-		unset($_SESSION['twitter']);
-		unset($this->twuser);
-		return $this->set('twuser',0,true);
-	}
+	/* 
+	 * link given Twitter account ID to the current user
+	 * it also checks whether it has already been linked
+	 * @param	twid	Twitter user ID, if false it will use currently logged in twitter account
+	 * @return	Whether the account was linked
+	 */
 	function linkTW($twid=false){
 		global $sess, $db, $tw;
 		if(!($sess instanceof Session)) return false;
@@ -360,16 +425,41 @@ class User{
 		}
 		return $this->set('twuser',$twid,true);
 	}
+	/*
+	 * unlinkTW for the current user
+	 * @return	Whether the account was unlinked
+	 */
+	function unlinkTW(){
+		if(!$this->tw()) return true;
+		global $sess, $db;
+		if(!($sess instanceof Session)) return false;
+		if(!($db instanceof DB)) $db = $sess->db();
+		if(!$this->fb() && !$this->g('email')){
+			// No puedes quedarte sin cuentas vinculadas
+			$sess->set_msg('Debes tener alguna cuenta vinculada siempre. Vincula Facebook o especifica una contraseña para poder desvincular Twitter');
+			return false;	
+		}
+		unset($_SESSION['user']['twuser']);
+		unset($_SESSION['twitter']);
+		unset($this->twuser);
+		return $this->set('twuser',0,true);
+	}
+	/*
+	 * Check if user has Facebook linked
+	 * @return	Facebook account ID or false if not linked
+	 */
 	function fb(){
-		// Return fbuser if has Facebook linked
 		$fb = $this->g('fbuser',true);
 		global $sess;
 		$sess->debug('fb='.$fb);
 		if(strlen($fb)>0 && $fb!=='0' && $fb!==0) return $fb;
 		return false;
 	}
+	/*
+	 * Check if user has Twitter linked
+	 * @return	Twitter account ID or false if not linked
+	 */
 	function tw(){
-		// Return twuser if has Twitter linked
 		$tw = $this->g('twuser',true);
 		global $sess;
 		$sess->debug('tw='.$tw);
